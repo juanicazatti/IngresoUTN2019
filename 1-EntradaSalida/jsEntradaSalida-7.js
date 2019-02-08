7e40ca2abb2adee2677859a038a9318ad18df1c1
juanicazatti <47321462+juanicazatti@users.noreply.github.com>
2019-02-08 12:29:26 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,22 +3,54 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{var num1;
+var num2;
+var resultado;
+num1 = document.getElementById("numeroUno").value;
+num2 = document.getElementById("numeroDos").value;
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+resultado = num1 + num2;
+alert ("la suma es: " + resultado);	
 	
 }
 
 function restar()
-{
+{var num1;
+var num2;
+var resultado;
+num1 = document.getElementById("numeroUno").value;
+num2 = document.getElementById("numeroDos").value;
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+resultado = num1 - num2;
+alert ("la resta es: " + resultado);
 	
 }
 
 function multiplicar()
-{ 
+{ var num1;
+var num2;
+var resultado;
+num1 = document.getElementById("numeroUno").value;
+num2 = document.getElementById("numeroDos").value;
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+resultado = num1 * num2;
+alert ("la multiplicación es: " + resultado);
 	
 }
 
 function dividir()
-{
+{var num1;
+var num2;
+var resultado;
+num1 = document.getElementById("numeroUno").value;
+num2 = document.getElementById("numeroDos").value;
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+resultado = num1 / num2;
+alert ("la división es: " + resultado);
 	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,14 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
-
+var sueldo;
+var aumento;
+var nuevosueldo;
+sueldo = document.getElementById("sueldo").value;
+aumento = document.getElementById("resultado").value;
+sueldo = parseInt(sueldo);
+aumento = parseInt(aumento);
+resultado = (sueldo * 10) / 100 ;
+alert ("su aumento es: " + resultado);
 	
 }
